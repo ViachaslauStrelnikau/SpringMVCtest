c8a19726b2c290b72c34611694df0d81b477bf5f
ViachaslauStrelnikau <damned.dementor@gmail.com>
2018-03-02 16:01:04 +0300

    add something )

--- src/main/java/com/test/DatabaseService.java
+++ src/main/java/com/test/DatabaseService.java
@@ -1,4 +1,9 @@
 package com.test;
 
+import model.Goods;
+import model.Invoice;
+
 public interface DatabaseService {
+    boolean addInvoice(Invoice invoice);
+    boolean addGoods(Goods goods);
 }
--- src/main/java/com/test/DatabaseServiceImpl.java
+++ src/main/java/com/test/DatabaseServiceImpl.java
@@ -1,13 +1,29 @@
 package com.test;
 
+import model.Goods;
+import model.Invoice;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+
 import javax.sql.DataSource;
 
 public class DatabaseServiceImpl implements DatabaseService{
 
-    private DataSource dataSource;
-
-    public void setDataSource(DataSource dataSource) {
-        this.dataSource = dataSource;
+    private JdbcTemplate jdbcTemplate;
+    private NamedParameterJdbcTemplate namedParameterJdbcTemplate;
+    public void setDataSource(DataSource dataSource)
+    {
+        this.jdbcTemplate = new JdbcTemplate(dataSource);
+        this.namedParameterJdbcTemplate=new NamedParameterJdbcTemplate(dataSource);
     }
-    
+
+    @Override
+    public boolean addInvoice(Invoice invoice) {
+        return false;
+    }
+
+    @Override
+    public boolean addGoods(Goods goods) {
+        return false;
+    }
 }
